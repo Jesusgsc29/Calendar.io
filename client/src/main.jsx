4a724e486670b4d4a32772e23be9087888e81d45
Jesusgsc29 <jesusgsc29@gmail.com>
2026-05-22 14:15:47 -0400

    Adding tailwindcss style

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-// import './index.css'; // if you use Tailwind
+import './index.css'; // Tailwind
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
     <App />
-    
+
   </React.StrictMode>
 );
